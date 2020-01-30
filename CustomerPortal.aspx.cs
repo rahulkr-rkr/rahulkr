2b09ac612726cec853a975a32eb2089e3850ee00
rahulkr-rkr <60231069+rahulkr-rkr@users.noreply.github.com>
2020-01-30 16:37:17 +0530

    Add files via upload

---
+++ CustomerPortal.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Configuration;
+using System.Data;
+
+
+public partial class CustomerPortal : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        lblWelcome.Text = Session["name"].ToString();
+    }
+
+
+   
+}
