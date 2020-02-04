a31ce258106e6f529a19eebe1f3efeb9fc665a0c
rahulkr-rkr <60231069+rahulkr-rkr@users.noreply.github.com>
2020-02-04 23:04:57 +0530

    Add files via upload

---
+++ INSproject/ins/CustomerClaim.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class CustomerClaim : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnClaim_Click(object sender, EventArgs e)
+        {
+            decimal a;
+            decimal b;
+            decimal c;
+            decimal d;
+            a = Convert.ToDecimal(tbClaimamount.Text);
+            b = Convert.ToDecimal(tbPaidamount.Text);
+            c = (a / b);
+            if(c < b)
+            {
+                d = a;
+                tbClaim.Text = (d.ToString().TrimEnd());
+            }
+            else if(c > b)
+            {
+                d = (a / 2) + (((a * 40) / 100));
+                tbClaim.Text = (d.ToString().TrimEnd());
+            }
+        }
+    }
+}
